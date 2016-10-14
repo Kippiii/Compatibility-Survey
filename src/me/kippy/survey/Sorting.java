d877af91efb87c2177914123b4488105bd66a594
Kippiii <ian.orzel@irchsonline.org>
2016-10-14 18:04:06 -0400

    Bug fixes.

--- src/me/kippy/survey/Sorting.java
+++ src/me/kippy/survey/Sorting.java
@@ -5,17 +5,17 @@ public class Sorting {
 public static void sort() {
 
 	for(int i = 0; i < Main.players.size(); i++) {
+		HashMap<Person, int> scores = new HashMap<Person, int>();
+		Player player1 = Main.players.get(i);
 		 for(int i1 = 0; i1 < Main.players.size(); i1++) {
-			Player player1 = Main.players.get(i);
 			Player player2 = Main.players.get(i1);
 			if(player1 != player2) {
-				int relationship = 0;
-				for(i2 = 0; i2 < Main.questions.size(); i++) {
-					if(player1.get(i2).equals(player2.get(i2))){
-						relationship = relationship +1;
+				int score = 0;
+				for(i2 = 0; i2 < Main.questions.size(); i2++) {
+					if(player1.get(i2) == player2.get(i2)){
+						score = score +1;
 					}	
 				}
-				player1.setComp(player2, relationship);
 			}
 		}
 	}

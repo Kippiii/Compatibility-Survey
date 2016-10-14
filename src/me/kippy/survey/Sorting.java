ca948d0f2b31225f7c6b44d420a49e54433edb5c
MAGENTAAUTUMN <david.kozdra@irchsonline.org>
2016-10-14 15:27:33 -0400

    Update Sorting.java

--- src/me/kippy/survey/Sorting.java
+++ src/me/kippy/survey/Sorting.java
@@ -4,9 +4,8 @@ public class Sorting {
 	
 public static void sort() {
 	
-	if (5< 8)
-	system.out.print("fuck you you will always be single you fat cow")
-		//Group 3 Giant balls 
+	
+		//Group 3
 		
 	}
 

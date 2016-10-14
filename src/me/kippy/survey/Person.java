d33531dcbdfda9f9f470522771377a71fdc3bb55
LuiCaliber <justin.rosart@irchsonline.org>
2016-10-14 15:55:24 -0400

    Update Person.java

--- src/me/kippy/survey/Person.java
+++ src/me/kippy/survey/Person.java
@@ -1,10 +1,17 @@
 package me.kippy.survey;
 
 public class Person {
-	String name;
+	ArrayList<String> answers = new ArrayList<String>();
 	
-	public Person(String name) {
-		name = this.name;
+	public Person() {
+		
+	}
+	
+	public String get(int i) {
+		return answers.get(i - 1);
+	public String set(int i, string s){
+		return answers.set(i - 1);
+	public String 
 	}
 
 }

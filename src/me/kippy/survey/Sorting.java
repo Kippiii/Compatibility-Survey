8311dcb8c2ad4465f8d3a683378a054a2046a001
Kippiii <ian.orzel@irchsonline.org>
2016-10-28 16:00:15 -0400

    Updates

--- src/me/kippy/survey/Sorting.java
+++ src/me/kippy/survey/Sorting.java
@@ -1,25 +1,31 @@
 package me.kippy.survey;
 
+import java.util.HashMap;
+
 public class Sorting {
 	
 public static void sort() {
 
 	for(int i = 0; i < Main.players.size(); i++) {
-		HashMap<Person, int> scores = new HashMap<Person, int>();
-		Player player1 = Main.players.get(i);
+		HashMap<Person, Integer> scores = new HashMap<Person, Integer>();
+		Person player1 = Main.players.get(i);
 		 for(int i1 = 0; i1 < Main.players.size(); i1++) {
-			Player player2 = Main.players.get(i1);
+			Person player2 = Main.players.get(i1);
 			if(player1 != player2) {
 				int score = 0;
-				for(i2 = 5; i2 <= 25; i2++) {
-					if(player1.get(i2) == player2.get(i2)){
+				for(int i2 = 5; i2 <= 25; i2++) {
+					if(player1.get(i2).equalsIgnoreCase(player2.get(i2))){
 						score = score +1;
 					}	
 				}
+				
+				scores.put(player2, score);
+				
 			}
 		}
+		 
+		 player1.setHash(scores);
+		 
 	}
-//then i will place it here
-//this is nice
-
+}
 }

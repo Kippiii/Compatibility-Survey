938f6688b3217ce1775bd6d7b7cebbf0658c81db
Kippiii <ian.orzel@irchsonline.org>
2016-10-28 15:59:25 -0400

    Updates

--- src/me/kippy/survey/Main.java
+++ src/me/kippy/survey/Main.java
@@ -2,13 +2,17 @@ package me.kippy.survey;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 
+import jxl.Cell;
 import jxl.Sheet;
 import jxl.Workbook;
 import jxl.read.biff.BiffException;
 
 public class Main {
-	
+
+	public static ArrayList<Person> players = new ArrayList<Person>();
+		
 	public static void read() throws IOException {
 		File iw = new File("");
 		Workbook w;
@@ -16,7 +20,14 @@ public class Main {
 			w = Workbook.getWorkbook(iw);
 			Sheet sheet = w.getSheet(0);
 			
-			//Group 2
+			for(int c = 1; c < sheet.getColumns(); c++) {
+				Person p = new Person();
+				players.add(p);
+				for(int r = 1; r < sheet.getRows(); r++) {
+					Cell cell = sheet.getCell(c, r);
+					p.set(r, cell.getContents());
+				}
+			}
 			
 		}catch(BiffException e) {
 			e.printStackTrace();

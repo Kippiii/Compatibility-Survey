2fa1de21832dcc0f85ffcbc5d71312131dbb0469
Masterofmalor <nicholas.fernandez@irchsonline.org>
2016-10-28 15:11:58 -0400

    Update Person.java

--- src/me/kippy/survey/Person.java
+++ src/me/kippy/survey/Person.java
@@ -9,8 +9,10 @@ public class Person {
 	
 	public String get(int i) {
 		return answers.get(i - 1);
+	}
 	public String set(int i, string s){
 		return answers.set(i - 1);
+	}
 	public String 
 	}
 

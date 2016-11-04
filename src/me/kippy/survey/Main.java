3d68c14049a551892ef0d86126efb3ea86c8ebdf
Kippiii <ian.orzel@irchsonline.org>
2016-11-04 15:54:58 -0400

    Adding during second meeting.

--- src/me/kippy/survey/Main.java
+++ src/me/kippy/survey/Main.java
@@ -3,6 +3,10 @@ package me.kippy.survey;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.Scanner;
+import java.util.TreeMap;
 
 import jxl.Cell;
 import jxl.Sheet;
@@ -14,18 +18,18 @@ public class Main {
 	public static ArrayList<Person> players = new ArrayList<Person>();
 		
 	public static void read() throws IOException {
-		File iw = new File("");
+		File iw = new File("F:\\Test.xls");
 		Workbook w;
 		try {
 			w = Workbook.getWorkbook(iw);
 			Sheet sheet = w.getSheet(0);
 			
-			for(int c = 1; c < sheet.getColumns(); c++) {
+			for(int r = 1; r < sheet.getRows(); r++) {
 				Person p = new Person();
 				players.add(p);
-				for(int r = 1; r < sheet.getRows(); r++) {
+				for(int c = 1; c < sheet.getColumns(); c++) {
 					Cell cell = sheet.getCell(c, r);
-					p.set(r, cell.getContents());
+					p.set(cell.getContents());
 				}
 			}
 			
@@ -41,6 +45,36 @@ public class Main {
 			e.printStackTrace();
 		}
 		Sorting.sort();
+	}
+	
+	public static void scan() {
+		
+		Scanner s = new Scanner(System.in);
+		String name = s.next();
+		
+		Person p = null;
+		for(int i = 0; i < players.size(); i++) {
+			if(players.get(i).get(1).equalsIgnoreCase(name)) {
+				p = players.get(i);
+				break;
+			}
+		}
+		
+		if(p != null) {
+			HashMap<Person, Integer> scores = p.getHash();
+			
+			TreeMap<Person, Integer> sortedScores = sort(scores);
+			System.out.println(sortedScores);
+			
+		}
+		
+	}
+	
+	public static TreeMap<Person, Integer> sort(HashMap<Person, Integer> map) {
+		Comparator<Person> c = new valueComparator(map);
+		TreeMap<Person, Integer> r = new TreeMap<Person, Integer>(c);
+		r.putAll(map);
+		return r;
 	}
 	
 }
